61e132de31be307455b6e82560f0d834d1e37f86
Mohamed Hassaan <mmhassaan3@gmail.com>
2021-12-08 23:42:46 +0400

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,19 @@ const toggleList = () => {
         navigator.style.display = "block"
     }
 }
-menu.addEventListener('click', toggleList)
+menu.addEventListener('click', toggleList);
+
+//Animation gsap
+const tl = gsap.timeline({defaults: {duration: .7, opacity: 0}});
+tl
+.from('.download-file', {stagger: "2", x: "-100%"})
+.from('.logo-name', {stagger: "2", x: "100%"})
+.from('.profession', {y: "-100%"})
+.from('.title-about', {y: "100%"})
+.from('.content-about', {y: "100%"})
+.from('.profile-image', {x: "-100%"}) 
+.from('.info', {y: "-100%"})
+
+
+
+
